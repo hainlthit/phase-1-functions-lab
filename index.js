bbd3652d35dc7714dbf76801a52431c19a4a967c
Hain Thit <hainlthit@gmail.com>
2021-12-14 18:32:49 -0500

    commit tree 788bc7ed136faa1155fa30ffd7c2a9d76bb85b94

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Code your solution in this file!
+
+const headquarters = 42;
+const feet = 264;
+
+function distanceFromHqInBlocks (num){
+
+    if (num > 42){
+    return num - headquarters;
+    }else if (num < 42){
+        return headquarters - num;
+    }
+
+}
+
+function distanceFromHqInFeet (num){
+    return distanceFromHqInBlocks(num) * feet;
+  
+}
+function distanceTravelledInFeet (headquarters, num2){
+  if (num2 > headquarters){
+      return (num2 - headquarters)*feet;
+
+  }else{
+  return (headquarters - num2)*feet;
+  }
+}
+
+function calculatesFarePrice(start, destination){
+  if (distanceTravelledInFeet(start, destination) <= 400){
+      return 0; }
+      else if (distanceTravelledInFeet(start,destination) < 2000 ){
+          return .02 * (distanceTravelledInFeet(start,destination) - 400);
+      }
+      else if (distanceTravelledInFeet(start,destination) > 2000 && distanceTravelledInFeet(start,destination) < 2500){
+          return 25;
+      }
+
+  else if (distanceTravelledInFeet(start,destination) > 2500) { return 'cannot travel that far';
+}
+}
+
+function calculatesFarePrice(start, destination){
+  let calculatesFarePrice;
+  let calculatesFeet= distanceTravelledInFeet(start, destination);
+ if (calculatesFeet < 400 ){
+      return calculatesFarePrice = 0;
+ } else if (calculatesFeet > 400 && calculatesFeet < 2000){
+     calculatesFarePrice = calculatesFeet - 400;
+     return (calculatesFarePrice * 0.02);
+ }
+ else if (calculatesFeet > 2000 && calculatesFeet < 2499){
+     calculatesFarePrice = 25;
+     return calculatesFarePrice;
+ }
+ else if (calculatesFeet > 2500){
+     return 'cannot travel that far';
+ }
+
+}
+
